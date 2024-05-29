82cc709c0300b9c665b8c9c96a575372522f8401
none <none>
2024-05-29 22:32:12 +0000

    2-mlflow

---
+++ 2-mlflow/train.py
@@ -0,0 +1,27 @@
+import os
+import pickle
+import mlflow
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.autolog()
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+def run_train(data_path: str):
+
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+    rf = RandomForestRegressor(max_depth=10, random_state=0)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_val)
+
+    rmse = mean_squared_error(y_val, y_pred, squared=False)
+
+
+if __name__ == '__main__':
+    run_train(data_path = "./out")
